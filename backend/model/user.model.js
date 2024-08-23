48d466b398a036c822828327aa86b3538b3989dd
chmuhammadasim <boybrave82@gmail.com>
2024-08-23 11:27:13 +0500

    feat: Update user model with additional fields
    The user model has been updated to include additional fields such as `lastLogin`, `isActive`, `education`, `experience`, `specialization`, and `patients`. These changes enhance the functionality of the user model by providing more comprehensive information for psychologists and improving the user experience.

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,5 +1,31 @@
 const mongoose = require('mongoose');
-
+// @typedef {Object} User
+// @property {Number} id - The unique identifier for the user.
+// @property {String} username - The username of the user.
+// @property {String} email - The email address of the user.
+// @property {String} password - The password of the user.
+// @property {String} role - The role of the user. Can be 'user', 'psychologist', or 'admin'.
+// @property {String} firstName - The first name of the user.
+// @property {String} lastName - The last name of the user.
+// @property {String} address - The address of the user.
+// @property {String} city - The city of the user.
+// @property {String} country - The country of the user.
+// @property {String} contact - The contact information of the user.
+// @property {String} bio - The biography of the user.
+// @property {String} profilePictureUrl - The URL of the user's profile picture.
+// @property {Date} dateOfBirth - The date of birth of the user.
+// @property {Date} createdAt - The date and time when the user was created.
+// @property {Date} lastLogin - The date and time of the user's last login.
+// @property {Boolean} isActive - Indicates whether the user is active or not.
+// @property {Array} games - An array of game IDs associated with the user.
+// @property {Array} questions - An array of question IDs associated with the user.
+// @property {Array} result - An array of result IDs associated with the user.
+// @property {Array} education - An array of degrees/certifications of the psychologist.
+// @property {String} experience - The description of the psychologist's experience.
+// @property {String} specialization - The area of expertise of the psychologist.
+// @property {Array} patients - An array of user IDs associated with the psychologist.
+// @property {String} psychologist - The ID of the psychologist associated with the user.
+ 
 const userSchema = new mongoose.Schema({
   id: {
     type: Number,
@@ -67,6 +93,13 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  lastLogin: {
+    type: Date,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
   games: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Game',
@@ -79,21 +112,28 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Result',
   }],
-  psychologist: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+  // Field specific to Psychologists
+  education: {
+    type: [String], // Array of degrees/certifications
+    trim: true,
+  },
+  experience: {
+    type: String, // Description of experience
+    trim: true,
+  },
+  specialization: {
+    type: String, // Psychologist's area of expertise
+    trim: true,
   },
   patients: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   }],
-  isActive: {
-    type: Boolean,
-    default: true,
+  // Field specific to Users
+  psychologist: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
   },
-  lastLogin: {
-    type: Date,
-  }
 });
 
 module.exports = mongoose.model('User', userSchema);
